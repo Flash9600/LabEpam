e139f2032905592fccbeeb102597cbed75d20971
Flash9600 <trefilovdimarus@gmail.com>
2021-03-18 12:17:15 +0400

    refactor: clear routing

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,14 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-	{
-		path: 'courses',
-		loadChildren: () => import('./core/core.module').then(m =>{
-			return m.CoreModule;
-			})
-	}
-];
+const routes: Routes = [];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],
