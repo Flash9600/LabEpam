95c360f678b7a512fb24f63907a2695a4f9dfc07
Flash9600 <trefilovdimarus@gmail.com>
2021-04-12 16:14:23 +0400

    fix: rename tests of duration pipe

--- project/src/app/core/pipes/duration-pipe/duration.pipe.spec.ts
+++ project/src/app/core/pipes/duration-pipe/duration.pipe.spec.ts
@@ -6,15 +6,15 @@ describe('DurationPipe', () => {
 		pipe = new DurationPipe();
 	});
 
-	it('transform should return duration in format "1h"', () => {
+	it('transform should return duration in format "1h", if incoming number % 60 = 0', () => {
 
 		expect(pipe.transform(60)).toBe('1h');
 	});
-	it('transform should return duration in format "1m"', () => {
+	it('transform should return duration in format "1m", if incoming number < 60', () => {
 
 		expect(pipe.transform(59)).toBe('59m');
 	});
-	it('transform should return duration in format "1h: 1m"', () => {
+	it('transform should return duration in format "1h: 1m", if incoming number > 60', () => {
 
 		expect(pipe.transform(61)).toBe('1h 1m');
 	});
